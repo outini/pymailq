ec9af45c782d6daf5d9e58a20b7ef9dcc28d1415
Denis 'jawa' Pompilio <denis.pompilio@nbs-system.com>
2014-04-30 15:42:23 +0200

    Adding Copyright and licence

--- pyqueueshell.py
+++ pyqueueshell.py
@@ -1,16 +1,28 @@
-#! /usr/bin/env python2.7
+#
+#    Postfix queue control python tool (pyqueue)
+#
+#    Copyright (C) 2014 Denis Pompilio (jawa) <denis.pompilio@gmail.com>
+#
+#    This file is part of pyqueue
+#
+#    This program is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License
+#    as published by the Free Software Foundation; either version 2
+#    of the License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-"""
-Postfix postqueue manipulation tool
-"""
-
-__docformat__ = 'restructuredtext en'
 __author__ = "Denis 'jawa' Pompilio"
-__credits__ = "Denis 'jawa' Pompilio"
-__license__ = "GPLv2"
-__maintainer__ = "Denis 'jawa' Pompilio"
 __email__ = "denis.pompilio@gmail.com"
-__status__ = "Development"
+__license__ = "GPLv2"
+__email__ = "denis.pompilio@gmail.com"
+__version__ = "0.4"
 
 import pyqueue
 import cmd
