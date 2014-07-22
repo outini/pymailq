a65375fcafde00114b046f42f13b8f8c38379ee9
Denis 'jawa' Pompilio <denis.pompilio@nbs-system.com>
2014-07-22 13:15:04 +0200

    setup.py is now Python3 compliant

--- setup.py
+++ setup.py
@@ -18,11 +18,18 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+import sys
 import os
 from distutils.core import setup
 
 if __name__ == '__main__':
-    release = file(os.path.join(os.path.dirname(__file__), "VERSION")).read()
+    if sys.version_info.major == 3:
+        release = open(os.path.join(
+                           os.path.dirname(__file__), "VERSION"), 'r').read()
+    else:
+        release = file(os.path.join(
+                           os.path.dirname(__file__), "VERSION")).read()
+
     setup(
         name = "pyqueue",
         version = ".".join(release.split('.')[0:2]),
