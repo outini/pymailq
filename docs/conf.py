e7720cb25df5a45bf83f61fa4d01634eb4c9e145
Denis 'jawa' Pompilio <denis.pompilio@nbs-system.com>
2017-09-01 18:28:06 +0200

    Modified conf.py for readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,10 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
+
+
 sys.path.insert(0, os.path.abspath("../pymailq"))
 sys.path.insert(0, os.path.abspath(".."))
 
@@ -94,7 +97,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'haiku'
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'haiku'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
