2ae22648a8a1fdfaad3dc5c8716e94f772887789
Denis 'jawa' Pompilio <denis.pompilio@nbs-system.com>
2017-09-12 13:12:08 +0200

    Adding tests runner

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,11 +1,46 @@
 #! /bin/sh
 
-# Call me like:
-# PYTHONPATH=. PATH="$PATH:./bin" tests/run_tests.sh
+usage() {
+    cat <<EOU
+Usage: $0 [-hs] [-e ENV]
 
-echo "Running python version:"
-python --version
+Options:
+    -h      show usage
+    -s      generate mail samples
+    -e ENV  use ENV as virtualenv (via pew) to run tests
+EOU
+    exit 1
+}
 
-echo "# Begin tests suite"
-pqshell < tests/commands.txt
-echo "# End tests suite"
+# --- Main ---
+
+gen_samples=false
+venv=
+
+while getopts :hse: opt; do
+    case "$opt" in
+        h) usage ;;
+        s) gen_samples=true ;;
+        e) venv=$OPTARG ;;
+        \?) echo "Unknown option $OPTARG"; exit 2 ;;
+        :) echo "Option -$OPTARG requires an argument."; exit 2 ;;
+    esac
+done
+
+# We're supposed to run in project's root
+[ -d tests ] || {
+    echo "Run me from project's root, tests directory not found."
+    exit 2
+}
+
+$gen_sample && {
+    echo "Generating samples."
+    sudo ./tests/generate_samples.sh
+}
+
+
+if [ pew_env ]; then
+    PYTHONPATH=. pew in "$venv" pytest -v ./tests/
+else
+    PYTHONPATH=. pytest -v ./tests/
+fi
